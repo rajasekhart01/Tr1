ca65febb0a57a7434fdd06ff90c3d4acb05adf0d
Raja Sekhar Thimmarayappagari <98815258+rajasekhart01@users.noreply.github.com>
2022-03-12 07:42:22 +0530

    Create add.c

---
+++ C++/add.c
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+  int a,b,c;
+  cout<< "Enter 2 integers \n";
+  cin>> a>>b;
+  c=a+b;
+  cout<<"addition of two numbers is \n";
+  cout<< c;
+  return 0;
+}
